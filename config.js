51e8391da8e63e7ab85470a32c5d2ae1f4e9e361
Mêrcy <78019226+mercyxrd@users.noreply.github.com>
2021-08-19 17:16:16 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    token: "",
+    prefx: "",
+    owner: "",
+    mongoURL: "",
+    sunucuID: "",
+    
+    roles: {
+        muted: "",
+    }, 
+
+    embed: {
+        sunucuAdı: "✬ V A L E N S A",
+        embedRenk: "",
+        authorTag: "Valensa ❤️ Mêrcy.",
+    },
+
+    staffs: {
+        banStaff: "",
+        muteStaff: "",
+    },
+
+    logs:{
+        mutelog: "",
+        jaillog: "",
+    },
+    
+    emojis: {
+        OnayE: "<a::869585811174199326>",
+        IptalE: "<a:red:869585801485377636>",
+    }
+}
