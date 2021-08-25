f91a8206b111ba4cad04a88587ea19df2fbf8507
Mêrcy <78019226+mercyxrd@users.noreply.github.com>
2021-08-25 11:54:14 +0300

    Update ban.js

--- SlashCommands/info/ban.js
+++ SlashCommands/info/ban.js
@@ -15,7 +15,6 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
         const user = await client.users.fetch(interaction.targetId)
-        if (!interaction.member.roles.cache.has(client.config.staffs.banStaff) && !interaction.member.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: "Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin"})
         if (interaction.guild.members.cache.has(user.id) && interaction.guild.members.cache.get(user.id).permissions.has(["VIEW_AUDIT_LOG","ADMINISTRATOR","MANAGE_GUILD","MANAGE_ROLES"])) return interaction.followUp({ content: `${client.config.emojis.IptalE} Bu kullanıcıyı yasaklayamazsın.`})
         interaction.guild.members.ban(user)
         interaction.followUp({ embeds: [new MessageEmbed().setAuthor(user.tag, user.avatarURL({dynamic: true})).setDescription(`${user} kullanıcısı ${interaction.user} tarafından **Belirtilmedi** sebebi ile yasaklandı.`)
